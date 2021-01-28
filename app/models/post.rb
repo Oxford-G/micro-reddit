1ef820ef25cce51ebef36bb95239dd0e36b4552c
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-01-28 15:58:34 +0100

    add user and post models

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 10 }
+  validates :body, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 50 }
+
+  belongs_to :user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 10 }
+  validates :password, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 10 }
+
+  has_many :posts
+end
